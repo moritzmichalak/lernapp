f4da29eef33136d23fa6b98fb4fa626fdda13792
moritzmichalak <52628841+moritzmichalak@users.noreply.github.com>
2025-06-25 22:50:46 +0200

    bilan hinzunehmen zum testen wdh falscher aufgaben

--- script.js
+++ script.js
@@ -2657,7 +2657,7 @@ function zeigeRezeptPinnwand() {
 
     // Automatisch IDs von schueler-30 bis schueler-43 erzeugen
     const userIds = [];
-    for (let i = 31; i <= 45; i++) {
+    for (let i = 31; i <= 46; i++) {
         userIds.push(`schueler-${i}`);
     }
 
