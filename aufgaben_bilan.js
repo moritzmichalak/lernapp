8f24258c3e7107ee80c35508586b6bd84fd47283
moritzmichalak <52628841+moritzmichalak@users.noreply.github.com>
2025-06-30 16:01:54 +0200

    Bilan 1. Aufgabe und Test eigenes Bild als Emoji

--- aufgaben_bilan.js
+++ aufgaben_bilan.js
@@ -78,7 +78,7 @@ const aufgaben_bilan = [
     // p. 65 ex 2 La fête
     {
         ueberschrift: "Test 1",
-        text: "Pour la salade, il me faut <img src='1kg.jpg' alt='1K Gewicht' width='24' height='24'></img> ➡️ ____  et ainsi",
+        text: "Pour la salade, il me faut  ➡️ ____  et ainsi",
         woerter: ["tout le", "toute la", "tous les", "toutes les"],
         korrekt: "toutes les"
     },
